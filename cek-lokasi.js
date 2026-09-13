a5a71fe3cea99175f9ab02c17cd3f619f288f9db
uuswalker <oktaarrijal@gmail.com>
2026-09-13 16:38:24 +0700

    Webhook ke deploy final notif lengkap AKfycbzLPiDPVkwHpkwhWOlT0iK9ku8w-wUOpxTVF9RvzsIPLiQhlfUqWQqoic2ZToXcqr4T

--- cek-lokasi.js
+++ cek-lokasi.js
@@ -44,7 +44,7 @@
 
 (function () {
   // ====== KONFIGURASI ======
-  var GOOGLE_SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbyFLCxHFvULrQbS2YO5qJjm0aaSo0PyrjTCDCqY0RjxCmrGCA5IZBEXfejCDlBikIuC/exec";
+  var GOOGLE_SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzLPiDPVkwHpkwhWOlT0iK9ku8w-wUOpxTVF9RvzsIPLiQhlfUqWQqoic2ZToXcqr4T/exec";
   var TOKEN = "xlsr_2026_s0lor4y4";
   var NOMOR_WA_SALES = "6287778999141";
 
