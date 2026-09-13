747471588e35ad8cf0135bd33f85f27363334820
uuswalker <oktaarrijal@gmail.com>
2026-09-13 15:21:03 +0700

    Webhook ke deploy Database Lead + Fonnte AKfycbyRAgJPGxaVVEKwShUU6fIjKZdWVRyHo3GIZavfSQJT-3cX84nyDf1DwTxylgnMjiMd

--- cek-lokasi.js
+++ cek-lokasi.js
@@ -44,7 +44,7 @@
 
 (function () {
   // ====== KONFIGURASI ======
-  var GOOGLE_SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzwT8I3B6-gWRqFnQ7WZy-cGcdv6MeROvNN6j071Ba3DTiIalHWS5ACzutC8xzMvcVa/exec";
+  var GOOGLE_SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbyRAgJPGxaVVEKwShUU6fIjKZdWVRyHo3GIZavfSQJT-3cX84nyDf1DwTxylgnMjiMd/exec";
   var TOKEN = "xlsr_2026_s0lor4y4";
   var NOMOR_WA_SALES = "6287778999141";
 
