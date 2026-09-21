f2d1de3e3b2228d4fc20025778c371065223f17a
uuswalker <oktaarrijal@gmail.com>
2026-09-22 02:47:14 +0700

    perf: eliminate lcp blocking animation and fix preload placement

--- components/animations/PageTransition.tsx
+++ components/animations/PageTransition.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function PageTransition({ children, ...props }: any) {
   return (
-    <main className="page-transition-enter" {...props}>
+    <main {...props}>
       {children}
     </main>
   );
